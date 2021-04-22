44697fa22285dd7a1dbe4aaed20b3431830152d8
qibowen1 <64078978+qibowen1@users.noreply.github.com>
2021-04-22 11:31:17 +0800

    2021-04-22 11:31:50

--- pages/functionpage/talking.js
+++ pages/functionpage/talking.js
@@ -1,44 +1,13 @@
-// pages/index/firstpage.js
-var app=getApp();
+// pages/functionpage/talking.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    imgs:[{
-      link:'../rotapage/rotapage1',
-      url:'../../image/tabericon/learn1.png'
-    },{
-      link:'../rotapage/rotapage2',
-      url:'../../image/tabericon/learn2.png'
-    },{
-      link:'../rotapage/rotapage3',
-      url:'../../image/tabericon/learn3.png'
-    },{
-      link:'../rotapage/rotapage4',
-      url:'../../image/tabericon/learn4.png'
-    }],
-    indicatorDots:true,
-    autoplay:true,
-    circular:true,
-    interval:3000,
-    vertical:false,
-    duration:1000,
 
-    functionContainer:[{
-      link:'../functionpage/myaims',
-      url:'../../image/tabericon/myaims.png',
-      id:1
-    },{
-      link:'../functionpage/talking',
-      url:'../../image/tabericon/talk.png',
-      id:2
-    }]
   },
-  catchTouchMove:function(e){
-    return true;
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
